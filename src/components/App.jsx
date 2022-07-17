cde77aa3eb3a036645b127d0329476aeac248b40
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-07-17 20:15:55 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,22 @@
-export const App = () => {
-  return (
+import React, { Component } from "react";
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+  goodFeedbackClick = () => {
+    this.setState({good: 1})
+  }
+  neutralFeedbackClick = () => {
+    this.setState({neutral: 1})
+  }
+  badFeedbackClick = () => {
+    this.setState({bad: 1})
+  }
+
+  render() {
+    return(
     <div
       style={{
         height: '100vh',
@@ -10,7 +27,21 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
-    </div>
-  );
+      <div className="feedbackBtns">
+      <h2>Please, leave your feedback</h2>
+      <button type="button" className="goodFeedbackBtn" onClick={this.goodFeedbackClick}>Good</button>
+      <button type="button"className="neutralFeedbackBtn" onClick={this.neutralFeedbackClick}>Neutral</button>
+      <button type="button" className="badFeedbackBtn" onClick={this.badFeedbackClick}>Bad</button>
+      </div>
+      <div className="statistics">
+      <h2>Statistics</h2>
+        <ul>
+          <li>Good:{this.state.good}</li>
+          <li>Neutral:{this.state.neutral}</li>
+          <li>Bad:{this.state.bad}</li>
+        </ul>
+      </div>
+
+    </div>)
+  };
 };
